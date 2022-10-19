03da1f4fdd312ece2c574fefd316e4e50984c598
Michael Lee <mhykol2114@gmail.com>
2022-10-19 13:15:08 -0700

    phase 1 complete?

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,16 +3,23 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
   });
+});
 
 router.get("/hello/world", function (req, res) {
   res.cookie("XSRF-TOKEN", req.csrfToken());
   res.send("Hello World!");
 });
+
+// backend/routes/index.js
+// ...
+const apiRouter = require("./api");
+
+router.use("/api", apiRouter);
+// ...
 
 module.exports = router;
