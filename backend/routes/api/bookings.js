6355324dbedf9f74d386890c9cbd1938bb426ba6
Michael Lee <mhykol2114@gmail.com>
2022-10-28 11:01:24 -0700

    finished bookings and deletes

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,105 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { SpotImage, Booking } = require("../../db/models");
+const { requireAuth } = require("../../utils/auth");
+
+const requireAuthor = async function (req, _res, next) {
+  const userId = await Booking.findByPk(req.params.bookingId, {
+    attributes: ["userId"],
+  });
+  if (userId) {
+    if (req.user.id === userId.toJSON().userId) return next();
+    else {
+      const err = new Error("Forbidden");
+      err.errors = ["Forbidden"];
+      err.status = 403;
+      return next(err);
+    }
+  } else {
+    const err = new Error("Couldn't find a Booking with the specified id");
+    err.title = "Couldn't find a Booking with the specified id";
+    err.errors = ["Booking couldn't be found"];
+    err.status = 404;
+    return next(err);
+  }
+};
+
+router.get("/current", requireAuth, async (req, res) => {
+  const userId = req.user.id;
+  const bookings = await Booking.findAll({ where: { userId } });
+  const Bookings = [];
+  for (let i = 0; i < bookings.length; i++) {
+    let booking = bookings[i];
+    let spot = await booking.getSpot({
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+    if (spot) {
+      spot = spot.toJSON();
+      let previewImage = await SpotImage.findOne({
+        where: { preview: true, spotId: spot.id },
+      });
+      spot.previewImage = previewImage ? previewImage.toJSON().url : null;
+      booking = booking.toJSON();
+      booking.Spot = spot;
+      Bookings.push(booking);
+    } else {
+      Bookings.push(booking);
+    }
+  }
+  return res.json({ Bookings });
+});
+
+router.put(
+  "/:bookingId",
+  requireAuth,
+  requireAuthor,
+  async (req, res, next) => {
+    const { endDate, startDate } = req.body;
+    if (endDate <= startDate) {
+      const err = new Error("Validation error");
+      err.title = "Validation error";
+      err.errors = ["endDate cannot be on or before startDate"];
+      err.status = 400;
+      return next(err);
+    }
+    const booking = await Booking.findByPk(req.params.bookingId);
+    booking.set({
+      startDate,
+      endDate,
+    });
+    booking.save();
+    res.json(booking);
+  }
+);
+
+//delete a booking
+router.delete(
+  "/:bookingId",
+  requireAuth,
+  requireAuthor,
+  async (req, res, next) => {
+    const booking = await Booking.findByPk(req.params.bookingId);
+    let startDate = booking.dataValues.startDate.getTime();
+    let endDate = booking.dataValues.endDate.getTime();
+    let currentDate = new Date();
+    currentDate = new Date(currentDate.toDateString()).getTime();
+
+    if (!(currentDate > startDate && currentDate < endDate)) {
+      booking.destroy();
+      res.json({
+        message: "Successfully deleted",
+        statusCode: res.statusCode,
+      });
+    } else {
+      const err = new Error("Bookings that have been started can't be deleted");
+      err.title = "Bookings that have been started can't be deleted";
+      err.errors = ["Bookings that have been started can't be deleted"];
+      err.status = 403;
+      return next(err);
+    }
+  }
+);
+
+module.exports = router;
