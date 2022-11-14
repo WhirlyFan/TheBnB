c4a691064f4fde70d18e72dedee5afa46badd398
Michael Lee <mhykol2114@gmail.com>
2022-11-14 08:37:13 -0800

    fixed sign up inputs and display spots

--- frontend/src/components/allSpots/index.js
+++ frontend/src/components/allSpots/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import * as spotsActions from "../../store/spots";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function AllSpots() {
- return (
+  const spots = useSelector((state) => state.spots.Spots);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(spotsActions.getAllSpotsThunk());
+  }, [dispatch]);
+
+  if (!spots) return null;
+
+  return (
     <div>
-        test
+      {Object.values(spots).map((spot) => {
+        return (
+          <div key={`spot-${spot.id}`}>
+            {`id: ${spot.id},  address: ${spot.address}, avgRating: ${spot.avgRating}, price: ${spot.price}`}
+          </div>
+        );
+      })}
     </div>
- )
+  );
 }
