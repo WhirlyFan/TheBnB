b266313c5a80851c663b5c4baceca2fde5ed4fd9
Michael Lee <mhykol2114@gmail.com>
2022-11-13 21:02:43 -0800

    sign up modal and spots store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 // frontend/src/App.js
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
+// import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import AllSpots from "./components/allSpots";
 
 function App() {
   const dispatch = useDispatch();
@@ -16,13 +16,7 @@ function App() {
   return (
     <>
       <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-        </Switch>
-      )}
+      <AllSpots />
     </>
   );
 }
---
+++ frontend/src/components/allSpots/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AllSpots() {
+ return (
+    <div>
+        test
+    </div>
+ )
+}
