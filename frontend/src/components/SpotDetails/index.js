7c03f5ea4f21f1bd09fae74040cf58fa63b0d344
WhirlyFan <michatl4@uci.edu>
2023-02-09 15:13:56 -0800

    moved reviews to it's own component

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -2,21 +2,16 @@ import React, { useEffect, useState } from "react";
 import * as spotsActions from "../../store/spots";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
-import * as reviewActions from "../../store/review";
 import "./SpotDetails.css";
 import Bookings from "../Bookings";
+import Reviews from "../Reviews";
 
 export default function SpotDetails() {
   const spot = useSelector((state) => state.spots.SpotDetails);
-  const reviews = useSelector((state) => state.review.Reviews);
   const user = useSelector((state) => state.session.user);
   const { spotId } = useParams();
   const dispatch = useDispatch();
   const history = useHistory();
-
-  const [review, setReview] = useState("");
-  const [stars, setStars] = useState("");
-  const [errors, setErrors] = useState([]);
 
   const [isLoaded, setIsLoaded] = useState(false);
   const [hasClicked, setHasClicked] = useState(false);
@@ -24,12 +19,7 @@ export default function SpotDetails() {
   useEffect(() => {
     dispatch(spotsActions.getSpotDetailsThunk(spotId))
       .then(() => {
-        dispatch(reviewActions.getSpotReviewsThunk(spotId));
-      })
-      .then(() => {
         setIsLoaded(true);
-        setReview("");
-        setStars("");
       })
       .catch(async (res) => {
         const data = await res.json();
@@ -39,36 +29,6 @@ export default function SpotDetails() {
 
   if (!spot) return null;
   if (!isLoaded) return null;
-
-  const clickEdit = (review, spotId) => {
-    history.push(`/spots/${spotId}/reviews/${review.id}/edit`);
-  };
-  const clickDelete = (reviewId) => {
-    dispatch(reviewActions.deleteSpotReviewThunk(reviewId)).then(() => {
-      setHasClicked(!hasClicked);
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setErrors([]);
-    dispatch(
-      reviewActions.createReviewThunk(
-        {
-          review,
-          stars,
-        },
-        spot.id
-      )
-    )
-      .then(() => {
-        setHasClicked(!hasClicked);
-      })
-      .catch(async (res) => {
-        const data = await res.json();
-        if (data && data.errors) setErrors(data.errors);
-      });
-  };
 
   let rating = spot.avgStarRating;
   rating = parseFloat(rating).toFixed(1);
@@ -104,99 +64,34 @@ export default function SpotDetails() {
         </div>
         <div className="spot-details-main">
           <div>
-            <h2>Spot hosted by {spot.Owner.firstName}</h2>
-            <hr></hr>
             <div>
-              <img
-                className="air-cover"
-                src="https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg"
-                alt="aircover"
-              ></img>
-              <p className="spot-details-overflow">
-                Every booking includes free protection from Host cancellations,
-                listing inaccuracies, and other issues like trouble checking in.sssssssssssssssssssss
-              </p>
+              <h2>Spot hosted by {spot.Owner.firstName}</h2>
+              <hr></hr>
+              <div>
+                <img
+                  className="air-cover"
+                  src="https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg"
+                  alt="aircover"
+                ></img>
+                <p className="spot-details-overflow">
+                  Every booking includes free protection from Host
+                  cancellations, listing inaccuracies, and other issues like
+                  trouble checking in.sssssssssssssssssssss
+                </p>
+              </div>
+              <hr></hr>
+              <p className="spot-details-overflow">{spot.description}</p>
+              <hr></hr>
             </div>
-            <hr></hr>
-            <p className="spot-details-overflow">{spot.description}</p>
-            <hr></hr>
           </div>
-        </div>
           <Bookings spot={spot} />
-        <div className="spot-reviews">
-          {reviews && Object.values(reviews).length > 0 ? (
-            <>
-              <h2>Reviews</h2>
-              <ul>
-                {Object.values(reviews).map((review) => {
-                  return (
-                    <div className={"review"} key={`review-${review.id}`}>
-                      <div>
-                        {review.User.firstName}{" "}
-                        {review.User.lastName.slice(0, 1)} · ★{review.stars}
-                      </div>
-                      <li>{review.review}</li>
-                      {user && user.id === review.userId && (
-                        <div className={"my-buttons"}>
-                          <button
-                            className="button"
-                            onClick={() => {
-                              clickEdit(review, spot.id);
-                            }}
-                          >
-                            Edit
-                          </button>
-                          <button
-                            className="button"
-                            onClick={() => {
-                              clickDelete(review.id);
-                            }}
-                          >
-                            Delete
-                          </button>
-                        </div>
-                      )}
-                    </div>
-                  );
-                })}
-              </ul>
-            </>
-          ) : (
-            <h2>No Reviews</h2>
-          )}
-          {user && (
-            <form className="add-review" onSubmit={handleSubmit}>
-              <ul>
-                {errors.map((error, idx) => (
-                  <li key={idx} className="error">
-                    {error}
-                  </li>
-                ))}
-              </ul>
-              <label>Review</label>
-              <textarea
-                type="text"
-                value={review}
-                onChange={(e) => setReview(e.target.value)}
-                required
-                placeholder="How was your stay?"
-              />
-              <label>Stars</label>
-              <input
-                type="number"
-                min="1"
-                max="5"
-                value={stars}
-                onChange={(e) => setStars(e.target.value)}
-                required
-                placeholder="Rate your stay from 1-5 stars!"
-              />
-              <button className="button" type="submit">
-                Add Review
-              </button>
-            </form>
-          )}
         </div>
+        <Reviews
+          spot={spot}
+          setHasClicked={setHasClicked}
+          hasClicked={hasClicked}
+          user={user}
+        />
       </div>
     </div>
   );
